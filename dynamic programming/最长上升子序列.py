c5e8da8ab7b098e5b49d61cc8b1a48f7897557a5
sk <2498256234@qq.com>
2018-05-16 11:31:19 +0800

    最长上升子序列

---
+++ dynamic programming/最长上升子序列.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+__date__ = '2018/5/16 9:33'
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = len(nums)
+        dp = [1 for _ in range(l)]
+        for i in range(1, l):
+            for j in range(0, i):
+                print(j, i)
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
+
+
+
+s = Solution()
+nums = [10, 9, 2, 5, 3, 4]
+s.lengthOfLIS(nums)
