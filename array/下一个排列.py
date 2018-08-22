18c208236aae6b8125ea79270464b1af64a23ec5
sk <2498256234@qq.com>
2018-08-22 11:53:03 +0800

    下一个排列

---
+++ array/下一个排列.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type numss: List[int]
+        :rtype: void Do not return anything, modify numss in-place instead.
+        """
+
+        partition = -1
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                partition = i
+                break
+
+        if partition == -1:
+            nums.reverse()
+            return
+        else:
+            for i in range(len(nums) - 1, partition, -1):
+                if nums[i] > nums[partition]:
+                    nums[i], nums[partition] = nums[partition], nums[i]
+                    break
+        left = partition + 1
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+
+s = Solution()
+
+nums = [3,2,1]
+s.nextPermutation(nums)
