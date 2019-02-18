95db59011a3df1c2395e91ad4c774ee01b295a6c
sksun <sunshengkai27@gmail.com>
2019-02-18 14:35:28 +0800

    leetcode 131

---
+++ binary tree/isSymmetric.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def helper(p, q):
+            if p == None and q == None:
+                return True
+            if p and q and p.val == q.val:
+                return helper(p.left, q.right) and helper(p.right, q.left)
+            return False
+
+        if root: return helper(root.left, root.right)
+        return True
+
--- dynamic programming/maxSubArray.py
+++ dynamic programming/maxSubArray.py
@@ -15,7 +15,36 @@ class Solution(object):
             m = max(current, m)
         return m
 
+    def maxSubArray2(self, nums):
+        """
+        分冶法
+        :param nums:
+        :return:
+        """
+        return self.helper(nums, 0, len(nums) - 1)
 
-nums = [-1]
+    def helper(self, nums, left, right):
+        if left == right: return nums[left]
+        mid = (left + right) / 2
+
+        left_max = sum_num = 0
+        for i in range(mid - 1, left - 1, -1):
+            sum_num += nums[i]
+            left_max = max(left_max, sum_num)
+
+        right_max = sum_num = 0
+        for i in range(mid + 1, right + 1):
+            sum_num += nums[i]
+            right_max = max(right_max, sum_num)
+
+        left_ans = self.helper(nums, left, mid - 1)
+        right_ans = self.helper(nums, mid + 1, right)
+
+        return max(left_max + nums[mid] + right_max, max(left_ans, right_ans))
+
+
+
+
+nums = [1, 2]
 s = Solution()
-print(s.maxSubArray(nums))
+print(s.maxSubArray2(nums))
