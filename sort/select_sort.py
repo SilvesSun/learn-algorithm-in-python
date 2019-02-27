70da317fa98e8804b121aa537999e8ac8be51e5f
sksun <sunshengkai27@gmail.com>
2019-02-27 12:10:29 +0800

    选择排序

--- sort/select_sort.py
+++ sort/select_sort.py
@@ -1,9 +1,12 @@
 def select_sort(alist):
     n = len(alist)
-    for i in range(n-1):
-        for j in range(i+1, n):
-            if alist[j] < alist[i]:
-                alist[i], alist[j] = alist[j], alist[i]
+    for i in range(0, n):
+        min = i
+        for j in range(i + 1, n):
+            if alist[j] < alist[min]:
+                min = j
+                alist[min], alist[i] = alist[i], alist[min]
+    return alist
 
 
 alist = [12, 2, 34, 78, 6]
