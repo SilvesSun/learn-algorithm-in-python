034cf907117f533ebcbb447383b1c6dac5a4204d
sk.sun <2498256234@qq.com>
2021-11-12 16:56:18 +0800

    update

--- dynamic programming/45_跳跃游戏II.py
+++ dynamic programming/45_跳跃游戏II.py
@@ -3,12 +3,16 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        m = max(nums)
         n = len(nums)
-        dp = [m] * n
-        dp[0] = 0
-        dp[1] = 1
-
+        max_pos, end_pos, step = 0, 0, 0
+        for i in range(n - 1):
+            # 从左到右遍历数组，到达边界时，更新边界并将跳跃次数增加 1
+            if max_pos >= i:  # 当前能到达的最远的距离
+                max_pos = max(max_pos, i + nums[i])
+                if i == end_pos: # 已到达边界
+                    end_pos = max_pos
+                    step += 1
+        return step
 
 if __name__ == '__main__':
     nums = [2, 3, 1, 1, 4]
