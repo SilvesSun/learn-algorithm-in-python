5b283588b23c2a227e05489f56d425e735cd48e1
sk.sun <2498256234@qq.com>
2021-11-23 17:53:41 +0800

    update

--- array/240_搜索二维矩阵II.py
+++ array/240_搜索二维矩阵II.py
@@ -46,6 +46,13 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        左下角的元素是这一行中最小的元素，同时又是这一列中最大的元素。比较左下角元素和目标：
+        若左下角元素等于目标，则找到
+        若左下角元素大于目标，则目标不可能存在于当前矩阵的最后一行，问题规模可以减小为在去掉最后一行的子矩阵中寻找目标
+        若左下角元素小于目标，则目标不可能存在于当前矩阵的第一列，问题规模可以减小为在去掉第一列的子矩阵中寻找目标
+        若最后矩阵减小为空，则说明不存在
+        """
         if not matrix:
             return False
         m = len(matrix)
