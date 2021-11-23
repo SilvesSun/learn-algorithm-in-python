90040c84c4922cafe20816b002bc648632672bf2
sk.sun <2498256234@qq.com>
2021-11-23 11:51:24 +0800

    update

--- array/384_打乱数组.py
+++ array/384_打乱数组.py
@@ -60,6 +60,9 @@ class Solution:
         return self.nums
 
     def shuffle(self) -> List[int]:
+        """
+        fish-yates shuffle算法
+        """
         for i in range(len(self.nums)):
             j = random.randrange(i, len(self.nums))
             self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
