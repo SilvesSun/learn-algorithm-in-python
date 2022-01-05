81e3711f2476ef149bd4e3e981353fcbf2aeddc0
sk.sun <2498256234@qq.com>
2022-01-05 16:17:17 +0800

    update

--- dynamic programming/42_接雨水.py
+++ dynamic programming/42_接雨水.py
@@ -38,3 +38,18 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        n = len(height)
+        left = [0] * n
+        right = [0] * n
+        for i in range(1, n):
+            left[i] = max(left[i - 1], height[i])
+        for i in range(n - 2, -1, -1):
+            right[i] = max(right[i + 1], height[i])
+        ans = 0
+        for i in range(1, n-1):
+            ans += min(left[i], right[i]) - height[i]
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().trap(height=[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
